ee79b7596da82083edbb6d4d30a0852beb8a0413
alsa07007 <alsa07007@E-PW05CGR5>
2024-11-13 08:35:32 +0100

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hyra bil!");
+            Console.WriteLine("Antal timmar du vill hyra en bil:");
+            int timmar = int.Parse(Console.ReadLine());
+            int total = timmar * 80;
+            Console.WriteLine("Detta kommer att kosta " + total + " kr.");
+        }
+    }
+}
